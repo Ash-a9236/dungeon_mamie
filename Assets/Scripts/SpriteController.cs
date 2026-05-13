333408fbab836324da02634c9f0a6bb9472470a1
SR-Delightfully <033.s.robinson@gmail.com>
2026-05-13 18:03:56 -0400

    updated player sprite flipping mechanics, press f5 to flip camera

--- Assets/Scripts/SpriteController.cs
+++ Assets/Scripts/SpriteController.cs
@@ -3,83 +3,159 @@ using UnityEngine;
 public class SpriteController : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] private bool SpriteSprite = true;
+    [SerializeField] private bool billboard = true;
     [SerializeField] private bool flipSprite = true;
 
     [Header("References")]
-    [SerializeField] private Transform target;
-    [SerializeField] private Rigidbody targetRigidbody;
+    [SerializeField] private Rigidbody targetRb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private CameraFollow cameraFollow;
+
+    [Header("Sprites")]
+    [SerializeField] private Sprite[] backSprites;
+    [SerializeField] private Sprite[] frontSprites;
 
     private Camera cam;
+    private bool lastFrontState;
 
     void Start()
     {
         cam = Camera.main;
 
-        if (!target)
-        {
-            target = transform.parent;
-        }
+        if (!targetRb)
+            targetRb = GetComponentInParent<Rigidbody>();
 
-        if (!targetRigidbody && target)
-        {
-            targetRigidbody =
-                target.GetComponent<Rigidbody>();
-        }
+        if (!animator)
+            animator = GetComponent<Animator>();
+
+        if (!spriteRenderer)
+            spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (!cameraFollow)
+            cameraFollow = FindObjectOfType<CameraFollow>();
+
+        if (cameraFollow)
+            lastFrontState = cameraFollow.faceFront;
     }
 
     void LateUpdate()
     {
-        HandleSprite();
-
+        HandleBillboard();
         HandleFlip();
+        HandleSpriteSwap();
+        HandleAnimations();
     }
 
-    void HandleSprite()
+    void HandleBillboard()
     {
-        if (!SpriteSprite)
+        if (!billboard || !cam)
             return;
 
-        if (!cam)
-            return;
-
-        transform.forward =
-            cam.transform.forward;
+        transform.forward = cam.transform.forward;
     }
 
     void HandleFlip()
     {
-        if (!flipSprite)
+        if (!flipSprite || !targetRb || !cam)
             return;
 
-        if (!targetRigidbody)
-            return;
-
-        Vector3 velocity =
-            targetRigidbody.velocity;
-
+        Vector3 velocity = targetRb.velocity;
         velocity.y = 0f;
 
         if (velocity.sqrMagnitude < 0.01f)
             return;
 
-        if (velocity.x > 0.05f)
+        Vector3 localVelocity =
+            cam.transform.InverseTransformDirection(velocity);
+
+        Vector3 scale = transform.localScale;
+
+        if (localVelocity.x > 0.05f)
+            scale.x = Mathf.Abs(scale.x);
+        else if (localVelocity.x < -0.05f)
+            scale.x = -Mathf.Abs(scale.x);
+
+        transform.localScale = scale;
+    }
+
+    void HandleSpriteSwap()
+    {
+        if (!cameraFollow || !spriteRenderer || !cam || !targetRb)
+            return;
+
+        Vector3 velocity = targetRb.velocity;
+        velocity.y = 0f;
+
+        if (velocity.sqrMagnitude < 0.01f)
+            return;
+
+        float forwardDot =
+            Vector3.Dot(velocity.normalized, cam.transform.forward);
+
+        bool currentFront = forwardDot > 0f;
+
+        if (currentFront == lastFrontState)
+            return;
+
+        lastFrontState = currentFront;
+
+        Sprite currentSprite = spriteRenderer.sprite;
+
+        int index = FindSpriteIndex(currentSprite);
+        if (index < 0) index = 0;
+
+        if (currentFront)
         {
-            transform.localScale =
-                new Vector3(
-                    Mathf.Abs(transform.localScale.x),
-                    transform.localScale.y,
-                    transform.localScale.z
-                );
+            if (index < frontSprites.Length)
+                spriteRenderer.sprite = frontSprites[index];
         }
-        else if (velocity.x < -0.05f)
+        else
         {
-            transform.localScale =
-                new Vector3(
-                    -Mathf.Abs(transform.localScale.x),
-                    transform.localScale.y,
-                    transform.localScale.z
-                );
+            if (index < backSprites.Length)
+                spriteRenderer.sprite = backSprites[index];
+        }
+    }
+
+    int FindSpriteIndex(Sprite sprite)
+    {
+        for (int i = 0; i < backSprites.Length; i++)
+            if (backSprites[i] == sprite)
+                return i;
+
+        for (int i = 0; i < frontSprites.Length; i++)
+            if (frontSprites[i] == sprite)
+                return i;
+
+        return -1;
+    }
+
+    void HandleAnimations()
+    {
+        if (!animator || !targetRb)
+            return;
+
+        Vector3 velocity = targetRb.velocity;
+        velocity.y = 0f;
+
+        float speed = velocity.magnitude;
+
+        bool moving = speed > 0.1f;
+
+        animator.SetBool("IsMoving", moving);
+        animator.SetFloat("Speed", speed);
+
+        bool isRunning = speed > 6f;
+        animator.SetBool("IsRunning", isRunning);
+        animator.SetBool("IsWalking", moving && !isRunning);
+        animator.SetBool("IsIdle", !moving);
+
+        PlayerController player =
+            GetComponentInParent<PlayerController>();
+
+        if (player)
+        {
+            animator.SetBool("IsDashing", player.isDashing);
         }
     }
 }
